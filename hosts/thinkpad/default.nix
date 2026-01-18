3267e140010e7b07c1d51dbb17938ce781d89fad
Pallaxis <sam_e3u@hotmail.com>
2026-01-18 23:27:59 +1300

    fix: thinkpad.nix fixes

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -2,38 +2,28 @@
 
 {
   imports = [
-    ./hardware-configuration.nix
-    ../../modules/core/default.nix
-    ../../modules/desktop/default.nix
+    ./hardware-configuration.nix                      # Auto generated hardware file
+    ./filesystem.nix                                  # Filesystem stuff that goes with hardware
+    ../../modules/core/default.nix                    # Loads all default files all systems should have
+    ../../modules/desktop/default.nix                 # Desktop stuff, hyprland etc.
+    ../../modules/hardware/nvidia.nix                 # Vendor specific settings
+    ../../modules/apps/other.nix
     inputs.home-manager.nixosModules.home-manager
   ];
 
-    home-manager = {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-      users.henry = import ../../users/henry/home.nix;
-    };
+  # Hostname
+  networking.hostName = "night";
 
-  networking.hostName = "thinkpad"; # Define your hostname.
+  # Temporary
   services.getty.autologinUser = "henry";
-  users.users.henry = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    home = "/home/henry";
-    shell = pkgs.zsh;
+
+  # Home Manager setup
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs; };
+    users.henry = import ../../users/henry/home.nix;
   };
-
-
-
-  ### SERVICES ###
-  services.xserver.xkb.layout = "us";
-  # fix to get trackpad to notice when I'm typing
-  environment.etc."libinput/local-overrides.quirks".text = pkgs.lib.mkForce ''
-    [Serial Keyboards]
-    MatchUdevType=keyboard
-    AttrKeyboardIntegration=internal
-    MatchName=keyd virtual keyboard
-  '';
 
   system.stateVersion = "25.11"; # Don't ever change this
 }
