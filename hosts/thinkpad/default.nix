6a34d2c40d3f23c9eeaca1ccefd17a60f90d928f
Pallaxis <sam_e3u@hotmail.com>
2026-02-09 10:21:27 +1300

    feat: keyd and libinput quirks

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -20,5 +20,13 @@
     SUBSYSTEM=="usb", ATTRS{idVendor}=="4255", MODE="0666"
   '';
 
+  services.keyd = {
+    keyboards.caps-swap.settings = {
+      main = {
+        sysrq = "layer(meta)";
+      };
+    };
+  };
+
   system.stateVersion = "25.11"; # Don't ever change this
 }
--- modules/core/etc-configs.nix
+++ modules/core/etc-configs.nix
@@ -1,9 +1,13 @@
-{ ... }:
-
-{
+{...}: {
   # sudo password timeout
   security.sudo.extraConfig = ''
     Defaults        timestamp_timeout=15
   '';
   nixpkgs.config.allowUnfree = true;
+  environment.etc."libinput/keyd-virtual-keyboard.quirks".text = ''
+    [Serial Keyboards]
+    MatchUdevType=keyboard
+    MatchName=keyd virtual keyboard
+    AttrKeyboardIntegration=internal
+  '';
 }
