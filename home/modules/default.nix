46902f7f309e3f789080f194dfc74517ae3d6449
Pallaxis <sam_e3u@hotmail.com>
2026-02-11 16:40:17 +1300

    chore: cleaning up home modules

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,25 +1,7 @@
-{...}: let
-  # cfg = config.my.host;
-in {
+{...}: {
   imports = [
     ./hyprland
     ./nvim
-    ./env.nix
-    ./shell.nix
+    ./terminal
   ];
-  config = {
-    #   my.home.modules =
-    #     {
-    #       networking.enable = true;
-    #     }
-    #     // lib.optionalAttrs (cfg.role == "desktop") {
-    #       # fonts.enable = true;
-    #       # my-user.enable = true;
-    #       # sound.enable = true;
-    #       hyprland.enable = true;
-    #       # system-recovery.enable = true;
-    #     }
-    #     // lib.optionalAttrs (cfg.role == "server") {
-    #     };
-  };
 }
