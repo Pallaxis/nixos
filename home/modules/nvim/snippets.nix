3f0668fb8d5b5e34cec4e8c777b96ce951086ef3
Pallaxis <sam_e3u@hotmail.com>
2026-02-12 19:49:53 +1300

    feat: way better nix module snippet called togglemod

--- home/modules/nvim/snippets.nix
+++ home/modules/nvim/snippets.nix
@@ -5,6 +5,16 @@
     local t = ls.text_node
     local i = ls.insert_node
 
+    -- Helper function to repeat the input from a specific index
+    local function copy(args)
+      return args[1][1]
+    end
+    local function capitalize(args)
+      local str = args[1][1]
+      return (str:gsub("^%l", string.upper))
+    end
+
+
     ls.add_snippets("nix", {
 
       s("newmod", {
@@ -20,6 +30,18 @@
         t({ "{", "  config,", "  lib,", "  pkgs,", "  ...", "}: {", "" }),
         i(0),
         t({ "", "}" }),
+      }),
+
+      s("togglemod", {
+        t({ "{", "  config,", "  lib,", "  pkgs,", "  ...", "}: let", "  cfg = config.my.modules." }),
+        i(1, "name"),
+        t({ ";", "in {", "  options.my.modules." }),
+        f(copy, { 1 }),
+        t({ ".enable =", '    lib.mkEnableOption "' }),
+        f(capitalize, { 1 }),
+        t({ '";', "", "  config = lib.mkIf cfg.enable {", "    " }),
+        i(0),
+        t({ "", "  };", "}" }),
       }),
 
     })
