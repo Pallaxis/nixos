309cf33709ceb75d0976d643f85e0d95f0a8b78b
Pallaxis <sam_e3u@hotmail.com>
2026-02-16 12:41:59 +1300

    fix: thinkpad workspace rules to use desc

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -26,16 +26,16 @@
             "desc:Dell Inc. DELL U2717D 7GGH576A330L, 2560x1440@60, auto-center-up, 1.25"
           ];
           workspaces = [
-            "1, monitor:DP-1, default:true"
-            "2, monitor:DP-1"
-            "3, monitor:DP-1"
-            "4, monitor:DP-1"
-            "5, monitor:DP-1"
-            "6, monitor:DP-2"
-            "7, monitor:DP-2"
-            "8, monitor:DP-2"
-            "9, monitor:DP-2"
-            "10, monitor:DP-2, default:true"
+            "1, monitor:desc:Dell Inc. DELL U2717D 7GGH576A330L, default:true"
+            "2, monitor:desc:Dell Inc. DELL U2717D 7GGH576A330L"
+            "3, monitor:desc:Dell Inc. DELL U2717D 7GGH576A330L"
+            "4, monitor:desc:Dell Inc. DELL U2717D 7GGH576A330L"
+            "5, monitor:desc:Dell Inc. DELL U2717D 7GGH576A330L"
+            "6, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x0536"
+            "7, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x0536"
+            "8, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x0536"
+            "9, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x0536"
+            "10, monitor:desc:InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x0536, default:true"
           ];
           exec-once = [
             "[workspace 5 silent] slack"
