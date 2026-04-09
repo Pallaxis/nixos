15928415a41ab47dc101d56dac2c6ca88fefdebf
Pallaxis <sam_e3u@hotmail.com>
2026-04-10 11:32:21 +1200

    feat: moved default.nix logic to henry.nix, also made it automatically import

--- home/henry.nix
+++ home/henry.nix
@@ -1,8 +1,50 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  osConfig,
+  lib,
+  ...
+}: {
   imports = [
     ./modules
   ];
 
+  my.home.modules =
+    {
+      ssh.enable = true;
+    }
+    // lib.optionalAttrs osConfig.my.modules.gaming.enable {
+      lutris.enable = true;
+    }
+    // lib.optionalAttrs (osConfig.my.host.name == "thinkpad") {
+    };
+  catppuccin = {
+    enable = true;
+    accent = "blue";
+    flavor = "mocha";
+
+    hyprlock.useDefaultConfig = false;
+    zsh-syntax-highlighting.enable = false;
+    thunderbird.profile = "henry";
+    tmux.extraConfig = ''
+      set -g @catppuccin_window_status_style "rounded"
+      set -ogq @catppuccin_window_text " #W"
+      set -ogq @catppuccin_window_current_text " #W"
+      set -g status-left ""
+      set -g status-right "#{?window_zoomed_flag,#[fg=#{@thm_mauve}]ZOOMED ,}#{E:@catppuccin_status_session}"
+    '';
+  };
+  programs.thunderbird = {
+    enable = true;
+    profiles = {
+      henry = {
+        isDefault = true;
+        settings = {
+          "mailnews.start_page.enabled" = false;
+          "calendar.week.start" = 1;
+        };
+      };
+    };
+  };
   home = {
     username = "henry";
     homeDirectory = "/home/henry";
--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,59 +1,7 @@
-{
-  osConfig,
-  lib,
-  ...
-}: {
-  imports = [
-    ./desktop
-    ./hyprland
-    ./lutris
-    ./niri
-    ./nvim
-    ./programs
-    ./scripts
-    ./services
-    ./ssh
-    ./terminal
-    ./waybar
-    ./xdg
-  ];
-  config = {
-    my.home.modules =
-      {
-        ssh.enable = true;
-      }
-      // lib.optionalAttrs osConfig.my.modules.gaming.enable {
-        lutris.enable = true;
-      }
-      // lib.optionalAttrs (osConfig.my.host.name == "thinkpad") {
-      };
-    catppuccin = {
-      enable = true;
-      accent = "blue";
-      flavor = "mocha";
-
-      hyprlock.useDefaultConfig = false;
-      zsh-syntax-highlighting.enable = false;
-      thunderbird.profile = "henry";
-      tmux.extraConfig = ''
-        set -g @catppuccin_window_status_style "rounded"
-        set -ogq @catppuccin_window_text " #W"
-        set -ogq @catppuccin_window_current_text " #W"
-        set -g status-left ""
-        set -g status-right "#{?window_zoomed_flag,#[fg=#{@thm_mauve}]ZOOMED ,}#{E:@catppuccin_status_session}"
-      '';
-    };
-    programs.thunderbird = {
-      enable = true;
-      profiles = {
-        henry = {
-          isDefault = true;
-          settings = {
-            "mailnews.start_page.enabled" = false;
-            "calendar.week.start" = 1;
-          };
-        };
-      };
-    };
-  };
+{lib, ...}: let
+  # Maps dirs in current dir to a list of paths
+  dirNames = lib.attrNames (lib.filterAttrs (name: type: type == "directory") (builtins.readDir ./.));
+  importPaths = map (name: ./. + "/${name}") dirNames;
+in {
+  imports = importPaths;
 }
