052219030ed318c38766e6f828336d542a321d12
Pallaxis <sam_e3u@hotmail.com>
2026-04-10 12:13:00 +1200

    feat: broke catppuccin out into its own module

--- home/henry.nix
+++ home/henry.nix
@@ -12,28 +12,13 @@
     {
       ssh.enable = true;
       thunderbird.enable = true;
+      catppuccin.enable = true;
     }
     // lib.optionalAttrs osConfig.my.modules.gaming.enable {
       lutris.enable = true;
     }
     // lib.optionalAttrs (osConfig.my.host.name == "thinkpad") {
     };
-  catppuccin = {
-    enable = true;
-    accent = "blue";
-    flavor = "mocha";
-
-    hyprlock.useDefaultConfig = false;
-    zsh-syntax-highlighting.enable = false;
-    thunderbird.profile = "henry";
-    tmux.extraConfig = ''
-      set -g @catppuccin_window_status_style "rounded"
-      set -ogq @catppuccin_window_text " #W"
-      set -ogq @catppuccin_window_current_text " #W"
-      set -g status-left ""
-      set -g status-right "#{?window_zoomed_flag,#[fg=#{@thm_mauve}]ZOOMED ,}#{E:@catppuccin_status_session}"
-    '';
-  };
   home = {
     username = "henry";
     homeDirectory = "/home/henry";
