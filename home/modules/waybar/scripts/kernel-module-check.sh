ea4bbc77206be26eca7b0d9fad4c754b6c1b6a14
Pallaxis <sam_e3u@hotmail.com>
2026-04-20 16:32:53 +1200

    fix: handles nixpkgs not including the patch version in their linux kernel version

--- home/modules/waybar/scripts/kernel-module-check.sh
+++ home/modules/waybar/scripts/kernel-module-check.sh
@@ -2,10 +2,11 @@
 
 current_kernel=$(uname -r)
 latest_kernel=$(nix eval --raw nixpkgs\#linuxPackages_latest.kernel.version)
+oldest_kernel_version=$(printf "%s\n%s" "$current_kernel" "$latest_kernel" | sort -V | head -n1)
 
-if [[ "$current_kernel" == "$latest_kernel" ]]; then
+if [[ "$current_kernel" != "$latest_kernel" ]] && [[ "$current_kernel" == "$oldest_kernel_version" ]]; then
+  echo "{\"text\": \" Kernel: $current_kernel\", \"tooltip\": \"Latest available: $latest_kernel\", \"class\": \"warning\"}"
   exit 0
 else
-  echo "{\"text\": \" Kernel: $current_kernel\", \"tooltip\": \"Latest available: $latest_kernel\", \"class\": \"warning\"}"
   exit 0
 fi
