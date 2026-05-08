a80b2b3f6a86cf3ec83ab116b1a2e45fe52435bb
Pallaxis <sam_e3u@hotmail.com>
2026-05-08 16:49:50 +1200

    feat: nvim gitsigns hunk binds

--- home/modules/nvim/plugins.nix
+++ home/modules/nvim/plugins.nix
@@ -141,13 +141,27 @@
             delete = { text = '_' },
             topdelete = { text = '‾' },
             changedelete = { text = '~' },
-          }
+          },
+          on_attach = function(bufnr)
+            local gitsigns = require('gitsigns')
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
+
+            map('n', '<leader>gh', gitsigns.preview_hunk, { desc = "[g]it [h]unk" })
+            map('n', '<leader>gB', gitsigns.blame, { desc = "[g]it [B]lame" })
+            map('n', '<leader>gn', function() gitsigns.nav_hunk('next') end, { desc = "[g]it [n]ext hunk" })
+            map('n', '<leader>gp', function() gitsigns.nav_hunk('prev') end, { desc = "[g]it [p]rev hunk" })
+          end
         }
       '';
     }
     {
       plugin = pkgs.vimPlugins.render-markdown-nvim;
       type = "lua";
+
       config = ''
         require('render-markdown').setup({
           code = { border = 'show' }
