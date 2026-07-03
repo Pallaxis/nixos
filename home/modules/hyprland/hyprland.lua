866b63251125ca1f3b390c3ad9aaa6a3341ad4ba
Pallaxis <sam_e3u@hotmail.com>
2026-07-03 16:25:17 +1200

    feat: turned off hyprland default wallpaper and splash

--- home/modules/hyprland/hyprland.lua
+++ home/modules/hyprland/hyprland.lua
@@ -229,6 +229,8 @@ hl.config({
   },
   misc = {
     initial_workspace_tracking = 2,
+    disable_hyprland_logo = true,
+    disable_splash_rendering = true,
   },
   xwayland = {
     force_zero_scaling = true,
