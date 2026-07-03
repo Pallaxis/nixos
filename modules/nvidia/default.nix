f4e7c6c10b1b6f55c2735c21a5100ba0a54d3613
Pallaxis <sam_e3u@hotmail.com>
2026-07-03 15:36:12 +1200

    feat: WIP nvidia early driver loading

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -15,5 +15,12 @@ in {
       nvidia.modesetting.enable = true;
     };
     services.xserver.videoDrivers = ["nvidia"];
+
+    # TODO: test me, should allow nvidia driver to work before unlock
+    # boot.initrd.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
+    # boot.kernelParams = [
+    #   "nvidia-drm.modeset=1"
+    #   "nvidia-drm.fbdev=1"
+    # ];
   };
 }
