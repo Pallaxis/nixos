a7e6fb86ea7716a339f8c79d69d9e60247f18f1b
Pallaxis <sam_e3u@hotmail.com>
2026-07-08 12:57:15 +1200

    feat: improved garbage collection, now a service

--- modules/garbage-collect/nixos.nix
+++ modules/garbage-collect/nixos.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.my.garbageCollect;
@@ -9,10 +10,22 @@ in {
     lib.mkEnableOption "GarbageCollect";
 
   config = lib.mkIf cfg.enable {
-    nix.gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
+    systemd.services."nix-env-delete-generations" = {
+      description = "Dereferences all but the last 5 nixos generations";
+      path = [pkgs.nix pkgs.bash];
+      serviceConfig = {
+        Type = "oneshot";
+        Restart = "on-failure";
+        ExecStart = "${pkgs.bash}/bin/bash -c 'nix-env --profile /nix/var/nix/profiles/system --delete-generations +5 && nix-collect-garbage'";
+      };
+    };
+    systemd.timers."nix-env-delete-generations" = {
+      description = "Daily timer for nix-env garbage collection";
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+      wantedBy = ["timers.target"];
     };
   };
 }
