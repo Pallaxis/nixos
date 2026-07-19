bf3f0fc7f9d8cfe7ba4ae6decdf44ebce12297d2
Pallaxis <sam_e3u@hotmail.com>
2026-07-20 09:29:26 +1200

    feat: added sops and age to core packages

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -14,6 +14,7 @@
   qt.enable = true;
 
   environment.systemPackages = with pkgs; [
+    age
     brightnessctl
     fd
     file
@@ -33,6 +34,7 @@
     pavucontrol
     python3
     quickshell
+    sops
     udisks
     unrar
     unzip
