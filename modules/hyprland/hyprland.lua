f4a0e74dd68c311adc4542e67ba9575034c68bf2
Pallaxis <sam_e3u@hotmail.com>
2026-07-19 19:10:10 +1200

    fix: night monitors

--- modules/hyprland/hyprland.lua
+++ modules/hyprland/hyprland.lua
@@ -63,8 +63,8 @@ local monitor_configs = {
     secondary = { output = "desc:Dell Inc. DELL U2715H 6VY7R735038S", mode = "2560x1440@60", position = "auto-center-up", scale = "1.25" },
   },
   night = {
-    primary = { output = "desc:Ancor Communications Inc ROG PG278Q #ASMObu8P/tXd", mode = "2560x1440@144", position = "0x0", scale = "1" },
-    secondary = { output = "DP-2", mode = "2560x1440@144", position = "-2560x0", scale = "1" },
+    primary = { output = "desc:Ancor Communications Inc ROG PG278Q #ASMObu8P/tXd", mode = "2560x1440@144", position = "-2560x0", scale = "1" },
+    secondary = { output = "desc:Samsung Electric Company LC32G7xT H4ZR900653", mode = "2560x1440@144", position = "0x0", scale = "1" },
   },
 }
 local config = monitor_configs[Hostname] or monitor_configs["default"]
