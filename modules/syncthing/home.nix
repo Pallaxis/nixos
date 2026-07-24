072451aa8653766b3281ded1a85b50dd99e543c5
Pallaxis <sam_e3u@hotmail.com>
2026-07-24 18:58:56 +1200

    fix: syncthing addresses changed to dynamic, not automatic

--- modules/syncthing/home.nix
+++ modules/syncthing/home.nix
@@ -13,15 +13,15 @@ in {
         settings = {
           devices = {
             iPhone = {
-              addresses = ["automatic"];
+              addresses = ["dynamic"];
               id = "UBH4QQR-EFVPO6H-TJTENLD-K7PSSAS-34KPRWJ-MNJ2CQY-65H3IN7-4633XAQ";
             };
             mist = {
-              addresses = ["automatic"];
+              addresses = ["dynamic"];
               id = "MCUYRFM-TSVNHCD-AXV45IH-3I7Z3D6-HKPP2BY-32IG4SS-WFHEPFT-VFEXEQ6";
             };
             homeserver = {
-              addresses = ["automatic"];
+              addresses = ["dynamic"];
               id = "4ENPKBG-HANSDQP-I4CC4XX-IVPRRNT-5CQ266C-VCLLK65-3RLAXNQ-Q5TT5AD";
             };
           };
