40a973f3f3295c066aa759b87c7d4daa99911239
Pallaxis <sam_e3u@hotmail.com>
2026-07-24 18:50:32 +1200

    feat: more syncthing devices added

--- modules/syncthing/home.nix
+++ modules/syncthing/home.nix
@@ -13,16 +13,22 @@ in {
         settings = {
           devices = {
             iPhone = {
-              addresses = [
-                "automatic"
-              ];
+              addresses = ["automatic"];
               id = "UBH4QQR-EFVPO6H-TJTENLD-K7PSSAS-34KPRWJ-MNJ2CQY-65H3IN7-4633XAQ";
+            };
+            mist = {
+              addresses = ["automatic"];
+              id = "MCUYRFM-TSVNHCD-AXV45IH-3I7Z3D6-HKPP2BY-32IG4SS-WFHEPFT-VFEXEQ6";
+            };
+            homeserver = {
+              addresses = ["automatic"];
+              id = "4ENPKBG-HANSDQP-I4CC4XX-IVPRRNT-5CQ266C-VCLLK65-3RLAXNQ-Q5TT5AD";
             };
           };
           folders = {
             "/home/${username}/share/Syncthing" = {
               id = "syncthing";
-              devices = ["iPhone"];
+              devices = ["iPhone" "mist" "homeserver"];
               versioning = {
                 type = "simple";
                 params = {
