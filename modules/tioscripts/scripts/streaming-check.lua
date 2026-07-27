cea94a0cb4d043134be68e3de33adf0cd72ac6d0
Pallaxis <sam_e3u@hotmail.com>
2026-07-28 09:15:16 +1200

    feat: minor tio script change

--- modules/tioscripts/scripts/streaming-check.lua
+++ modules/tioscripts/scripts/streaming-check.lua
@@ -6,24 +6,54 @@ end
 tio.write("\n")
 
 while true do
-  -- Puts SoM into EVK2 mode for this boot only SoM only for others
+  --   -- Puts SoM into EVK2 mode for this boot only SoM only for others
+  --   while true do
+  --     tio.write("t")
+  --     if tio.readline():find("5. Continue booting") then
+  --       print("PASSED")
+  --       tio.write("2\n")
+  --       tio.msleep("200")
+  --       tio.write("4\n")
+  --       tio.msleep("200")
+  --       tio.write("5\n")
+  --       break
+  --     end
+  --   end
+
+  -- while not tio.expect("~]# ", 1000) do
+  --   if tio.expect("login: ", 1000) then
+  --     tio.write("root\n")
+  --     if not tio.expect("~]# ", 5000) then
+  --       print("Root login failed or shell did not appear")
+  --     end
+  --   else
+  --     tio.write("\n")
+  --     print("else statement")
+  --   end
+  -- end
+
   while true do
-    tio.write("t")
-    if tio.readline():find("5. Continue booting") then
-      print("PASSED")
-      tio.write("2\n")
-      tio.msleep("200")
-      tio.write("4\n")
-      tio.msleep("200")
-      tio.write("5\n")
-      break
+    local line, partial = tio.readline(1000)
+    local data = line or partial
+
+    if data then
+      print("RX: " .. data)
+
+      if data:match("~%]#") then
+        print("Shell ready")
+        break
+      elseif data:match("login:") then
+        print("Logging in")
+        tio.write("root\n")
+        tio.expect("~]#")
+        break
+      end
+    else
+      -- no line received, wake device
+      print("No new line")
+      tio.write("\n")
     end
   end
-
-  tio.expect("login:")
-  tio.write("root\n")
-  shell_ready()
-  tio.write("clear\n")
 
   -- Grabs serial
   tio.write("oclea_info\n")
@@ -58,9 +88,13 @@ while true do
   end
 
   -- Provision sensor, modprobe driver
-  -- IMX327
 
+  -- IMX327
   -- tio.write("hydra_provision -i 0 -s 0x0101 0x0103 -s 0x0401 0x0780 -s 0x0402 0x0438 -s 0x0403 0x001e -s 0x0103 0x0101\n")
+
+  -- IMX415
+  tio.write("hydra_provision -i 0 -s 0x0101 0x0106 -s 0x0401 0x0f00 -s 0x0402 0x0870 -s 0x0403 0x001e -s 0x0103 0x0101 -s 0x0102 0x0037\n")
+
   -- tio.expect("~]# ")
 
   tio.write("hydra\n")
@@ -70,15 +104,10 @@ while true do
   tio.write("rm ~/.bash_history\n")
   tio.expect("~]# ")
 
-  tio.write("tek_ota -D 0\n")
-  tio.expect("~]# ")
-
   -- Start and pick up stream with ffplay storing pid for later
   tio.write("oclea_gstreamer_interactive_example -r\n")
   tio.expect(">>>")
-  local command = "sh -c 'setsid ffplay -fflags nobuffer -flags low_delay -probesize 32 -analyzeduration 1 rtsp://"
-    .. ip
-    .. ":8554/test >/dev/null 2>&1 & echo $!'"
+  local command = "sh -c 'setsid ffplay -fflags nobuffer -flags low_delay -probesize 32 -analyzeduration 1 rtsp://" .. ip .. ":8554/test >/dev/null 2>&1 & echo $!'"
   print(command)
   local fh = io.popen(command, "r")
   local pid = tonumber(fh:read("*l"))
