7ec4c740534d343d0f10feac46a5ecb639b2047d
Pallaxis <sam_e3u@hotmail.com>
2026-08-21 15:40:49 +1200

    fix: zombie syncthing-devices targets homeManager class

--- modules/hosts/zombie [N]/services/syncthing-devices.nix
+++ modules/hosts/zombie [N]/services/syncthing-devices.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.syncthing = {
+  flake.modules.homeManager.syncthing = {
     services.syncthing.settings.devices = {
       zombie.id = "LDYSISA-6QSGMJM-ABOMPAD-LOTPXJU-K4YVCFY-T6SOQ67-FKE6ZPJ-NGRUPQB";
     };
