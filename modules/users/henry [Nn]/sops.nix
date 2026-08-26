10d3c2efe93e348fae48be6d41da2264393c4b23
Pallaxis <sam_e3u@hotmail.com>
2026-08-26 12:48:41 +1200

    feat: renaming sops keys to be more accurate

--- modules/users/henry [Nn]/sops.nix
+++ modules/users/henry [Nn]/sops.nix
@@ -2,7 +2,7 @@
   flake.modules.homeManager.sops = {config, ...}: {
     sops = {
       age.keyFile = "${config.xdg.configHome}/sops/age/henry-age-key.txt";
-      defaultSopsFile = ../../../secrets/core.yaml;
+      defaultSopsFile = ../../../secrets/henry.yaml;
     };
   };
 }
