2d3280ff08924ec7cb0f020cebbe690087769de9
Pallaxis <sam_e3u@hotmail.com>
2026-08-26 19:48:07 +1200

    chore: remade system secrets, needed new key

--- modules/programs/nixflix [N]/secrets.nix
+++ modules/programs/nixflix [N]/secrets.nix
@@ -1,37 +1,37 @@
 {
-  flake.modules.nixos.nixflix = {config, ...}: let
-    admin = "henry";
-  in {
+  flake.modules.nixos.nixflix = {
     sops = {
-      age.keyFile = "/etc/sops/age/homelab-age-key.txt";
-      defaultSopsFile = ../../../secrets/homelab.yaml;
-      secrets = {
-        "qbittorrent/password" = {};
-        "qbittorrent/password_hash" = {};
-        "sonarr/api_key" = {};
-        "sonarr/password" = {};
-        "radarr/api_key" = {};
-        "radarr/password" = {};
-        "lidarr/api_key" = {};
-        "lidarr/password" = {};
-        "prowlarr/api_key" = {};
-        "prowlarr/password" = {};
-        "jellyfin/api_key" = {};
-        "jellyfin/password" = {};
-        "seerr/api_key" = {};
-        "wireguard/conf" = {};
-        # "sabnzbd/api_key" = {};
-        # "sabnzbd/nzb_key" = {};
-        # "sabnzbd/username" = {};
-        # "sabnzbd/password" = {};
-        # "usenet/eweka/username" = {};
-        # "usenet/eweka/password" = {};
-        # "usenet/newsgroupdirect/username" = {};
-        # "usenet/newsgroupdirect/password" = {};
-        # "indexer-api-keys/DrunkenSlug" = {};
-        # "indexer-api-keys/NZBFinder" = {};
-        # "indexer-api-keys/NzbPlanet" = {};
-      };
+      secrets = builtins.listToAttrs (map (name: {
+          inherit name;
+          # NOTE: homelab key must be merged into system-core-age-key.txt for this to work
+          value = {sopsFile = ../../../secrets/homelab.yaml;};
+        }) [
+          "qbittorrent/password"
+          "qbittorrent/password_hash"
+          "sonarr/api_key"
+          "sonarr/password"
+          "radarr/api_key"
+          "radarr/password"
+          "lidarr/api_key"
+          "lidarr/password"
+          "prowlarr/api_key"
+          "prowlarr/password"
+          "jellyfin/api_key"
+          "jellyfin/password"
+          "seerr/api_key"
+          "wireguard/conf"
+          # "sabnzbd/api_key"
+          # "sabnzbd/nzb_key"
+          # "sabnzbd/username"
+          # "sabnzbd/password"
+          # "usenet/eweka/username"
+          # "usenet/eweka/password"
+          # "usenet/newsgroupdirect/username"
+          # "usenet/newsgroupdirect/password"
+          # "indexer-api-keys/DrunkenSlug"
+          # "indexer-api-keys/NZBFinder"
+          # "indexer-api-keys/NzbPlanet"
+        ]);
     };
   };
 }
