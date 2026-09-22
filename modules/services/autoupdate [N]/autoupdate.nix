c5ef0f339ba0b03933355d1d4b2abe1e72205d2c
Pallaxis <sam_e3u@hotmail.com>
2026-09-23 08:21:03 +1200

    feat: autoupdate notification

--- modules/services/autoupdate [N]/autoupdate.nix
+++ modules/services/autoupdate [N]/autoupdate.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: {
-  flake.modules.nixos.autoupdate = {
+  flake.modules.nixos.autoupdate = {pkgs, ...}: {
     # auto updates system from github repo
     # as long as current generation is clean
     system.autoUpgrade = {
@@ -17,6 +17,26 @@
         RestartSec = "20";
         CPUSchedulingPolicy = "idle";
         IOSchedulingClass = "idle";
+        ExecStartPre = [
+          "${(pkgs.writeShellApplication {
+            name = "notify-upgrade";
+            runtimeInputs = with pkgs; [coreutils libnotify util-linux];
+            text = ''
+              # Notify the logged-in user that a system update is underway.
+              # Falls back to a no-op when no desktop session bus is available.
+              for bus in /run/user/*/bus; do
+                [ -S "$bus" ] || continue
+                uid=''${bus#/run/user/}
+                uid=''${uid%%/*}
+                runuser -u "$uid" -- \
+                  env DBUS_SESSION_BUS_ADDRESS="unix:path=$bus" XDG_RUNTIME_DIR="''${bus%/bus}" \
+                  timeout 10 notify-send -a nixos-upgrade "System update" "A system update is underway" \
+                  > /dev/null 2>&1 &
+              done
+              exit 0
+            '';
+          })}/bin/notify-upgrade"
+        ];
       };
     };
   };
